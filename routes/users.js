b64ab8b7463f48fef39892fd27dcd303dfc14c9a
LinkRober <1246983559@qq.com>
2020-11-18 18:28:05 +0800

    initial

---
+++ routes/users.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  var trump = {name:'Trump',age:'66'};
+  res.header('content-type','application/json;charset=utf-8');
+  res.cookie('rememberme', '1', { expires: new Date(Date.now() + 900000), httpOnly: true });
+  res.send(trump);
+});
+
+module.exports = router;
