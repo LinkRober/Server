26fde7a1ef894718e3b0a1830f108bdc57946603
LinkRober <1246983559@qq.com>
2020-11-19 19:47:21 +0800

    a simple event

--- page/test.js
+++ page/test.js
@@ -1,4 +1,32 @@
-const { use } = require("../routes");
+// const { use } = require("../routes");
 
-'use strict';
-console.log('window inner size: ' + window.innerWidth + ' x ' + window.innerHeight);
+// 'use strict';
+// var request = new XMLHttpRequest();
+  // function(window,undefined){
+
+  // })
+// window.onload = funtion() {
+//   function changeValue(value) {
+//     console.log(value);
+//     var p = document.getElementById('content');
+//     p.innerHTML = value;
+//   }
+//   changeValue("我是结果");
+// }
+
+// (window.onload() = function(){
+//     function changeValue(value) {
+//     console.log(value);
+//     var p = document.getElementById('content');
+//     p.innerHTML = value;
+//   }
+//   changeValue("我是结果");
+// })
+(function(window, doc){
+  function changeValue(value) {
+    console.log(value);
+    var p = document.getElementById('content');
+    p.innerHTML = value;
+  }
+  changeValue("我是结果");
+})(window, document);
