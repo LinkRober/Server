d36d69486a09988227b6ca6a5c7eaf30b9059539
LinkRober <1246983559@qq.com>
2020-11-20 17:28:40 +0800

    update file

--- routes/users.js
+++ routes/users.js
@@ -5,6 +5,8 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   var trump = {name:'Trump',age:'66'};
   res.header('content-type','application/json;charset=utf-8');
+  //Access-Control-Allow-Origin
+  res.header('Access-Control-Allow-Origin','*');
   res.cookie('rememberme', '1', { expires: new Date(Date.now() + 900000), httpOnly: true });
   res.send(trump);
 });
